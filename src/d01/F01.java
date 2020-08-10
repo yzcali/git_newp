481566c57a54be7849f1b26870bcab2b648a70cd
yzcali <yaziciali694@gmail.com>
2020-08-10 11:34:02 +0200

    third commit

--- src/d01/F01.java
+++ src/d01/F01.java
@@ -5,6 +5,9 @@ public class F01 {
 	public static void main(String[] args) {
 	  System.out.println("alex writer yazar");
        System.out.println("alex writer ingiltere de dogdu");
+	System.out.println("alex writer yuzden fazla kitap yazdi");
+	
+	
 	}
 
 }
